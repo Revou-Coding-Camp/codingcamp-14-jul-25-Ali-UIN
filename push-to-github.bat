473f31fee6204bf789c4ca8aeb21b4ba5bcb1369
Muhammad Irfan Ali <muhammad.irfan.ali24058@mhs.uingusdur.ac.id>
2025-07-18 21:56:54 +0700

    Initial commit: Add Muhammad Irfan Ali portfolio website

---
+++ push-to-github.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Pushing to GitHub...
+cd "c:\Users\Admin\New folder (2)"
+git push -u origin master
+echo.
+echo Done! Your website is now on GitHub.
+echo.
+echo Next steps:
+echo 1. Go to your GitHub repository
+echo 2. Click on "Settings"
+echo 3. Scroll down to "Pages"
+echo 4. Select "Deploy from a branch" 
+echo 5. Choose "master" branch
+echo 6. Click "Save"
+echo.
+echo Your website will be available at:
+echo https://YOUR-USERNAME.github.io/muhammad-irfan-ali-portfolio
+pause
